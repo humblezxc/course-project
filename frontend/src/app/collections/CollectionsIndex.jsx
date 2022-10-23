032dbaf33b097dde5631e75aa9bb1ca5e5ce86b2
humblezxc <you@example.com>
2022-10-23 21:13:01 +0200

    Added Collection items index

---
+++ frontend/src/app/collections/CollectionsIndex.jsx
@@ -0,0 +1,51 @@
+// Create Collection Index Component
+// Import React
+// use mui cards component to display collection cards
+// each card should have a name and description.
+// each card should have a button to view the collection and to edit and destroy the collection
+import * as React from 'react';
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Card, CardActionArea, CardContent, List, ListItem, ListItemText, Typography} from "@mui/material";
+import Button from "@mui/material/Button";
+import routes from "../../shared/constants/routes";
+
+export default function collectionIndex() {
+    const [collection, setCollection] = useState([]);
+    useEffect(() => {
+        axios.get("/collections")
+            .then(res => {
+                setCollection(res.data);
+            })
+            .catch(err => console.log(err));
+
+    }, []);
+
+    return (
+        <div>
+            <h1>Collection Index</h1>
+            <List>
+                {collection.map((collection) => (
+                    <ListItem key={collection.id}>
+                        <Card sx={{width: 300, height: 300}}>
+                            <CardActionArea>
+
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    { collection.collectionName}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {collection.description}
+                                </Typography>
+                            <Button variant="contained" href={routes.COLLECTIONS + "/" + collection.id}>View</Button>
+                            <Button variant="contained" href={routes.COLLECTIONS + "/" + collection.id + "/edit"}>Edit</Button>
+                            <Button variant="contained" href={routes.COLLECTIONS + "/" + collection.id + "/delete"}>Delete</Button>
+                            </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+}
--- frontend/src/common/routes/AppRoutes.js
+++ frontend/src/common/routes/AppRoutes.js
@@ -4,6 +4,7 @@ import Login from "../../app/auth/Login";
 import Home from "../../app/home/Home";
 import Register from "../../app/auth/Register";
 import CreateCollection from "../../app/collections/CreateCollection"
+import Collections from "../../app/collections/CollectionsIndex"
 import CreateItem from "../../app/collections/items/CreateItem"
 import routes from "../../shared/constants/routes";
 
@@ -13,6 +14,7 @@ export default function AppRoutes() {
             <Route exact path={routes.HOME} element={<Home />} />
             <Route path={routes.LOGIN} element={<Login />} />
             <Route path={routes.REGISTER} element={<Register />} />
+            <Route path={routes.COLLECTIONS} element={<Collections />} />
             <Route path={routes.COLLECTION_CREATE} element={<CreateCollection />} />
             <Route path={routes.ITEM_CREATE} element={<CreateItem/>} />
         </Routes>
