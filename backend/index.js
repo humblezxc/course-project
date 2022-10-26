3dd35a5e684f5b2a206548e62ed28b3e9f81e65d
humblezxc <you@example.com>
2022-10-26 23:57:34 +0200

    Heroku fix

--- backend/index.js
+++ backend/index.js
@@ -20,8 +20,21 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
-});
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+// });
+let defended = ['transformed.js', 'main.css', 'favicon.ico']
 
+app.get("*", (req, res) => {
+
+    let path = req.params['0'].substring(1)
+
+    if (defended.includes(path)) {
+        // Return the actual file
+        res.sendFile(`${__dirname}/build/${path}`);
+    } else {
+        // Otherwise, redirect to /build/index.html
+        res.sendFile(`${__dirname}/build/index.html`);
+    }
+});
 app.listen(process.env.PORT || 5000, ()=> console.log(`Server running at port ${process.env.PORT}`));
