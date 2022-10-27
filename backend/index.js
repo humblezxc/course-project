93722695ee337b6d8c3ecbed16aacb5e38e6ab14
humblezxc <you@example.com>
2022-10-27 18:18:01 +0200

    Heroku fix

--- backend/index.js
+++ backend/index.js
@@ -20,43 +20,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
-// });
+app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
+app.use(express.static("public"));
 
-// List of all the files that should be served as-is
-app.get("*", (req, res) => {
-    let url = path.join(__dirname, '../frontend/build', 'index.html');
-    if (!url.startsWith('/app/')) // since we're on local windows
-        url = url.substring(1);
-    res.sendFile(url);
-});
-
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("../frontend/build"));
-//     app.get("/*", function(req, res) {
-//         res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-//     });
-// }
-//
-// else {
-//     app.use(express.static(path.join(__dirname, '../frontend/public')));
-//     app.get("/*", function(req, res) {
-//         res.sendFile(path.join(__dirname, "../frontend/public/index.html"));
-//     });
-// }
-// let defended = ['transformed.js', 'main.css', 'favicon.ico']
-//
-// app.get("*", (req, res) => {
-//
-//     let path = req.params['0'].substring(1)
-//
-//     if (defended.includes(path)) {
-//         // Return the actual file
-//         res.sendFile(`${__dirname}/build/${path}`);
-//     } else {
-//         // Otherwise, redirect to /build/index.html
-//         res.sendFile(`${__dirname}/build/index.html`);
-//     }
-// });
 app.listen(process.env.PORT || 5000, ()=> console.log(`Server running at port ${process.env.PORT}`));
