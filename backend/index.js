0bcef2b5f7c32048fe5607ee55a534cad745d110
humblezxc <you@example.com>
2022-10-27 11:09:13 +0200

    Heroku fix

--- backend/index.js
+++ backend/index.js
@@ -23,18 +23,32 @@ app.use(router);
 // app.get('/*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
 // });
-let defended = ['transformed.js', 'main.css', 'favicon.ico']
 
-app.get("*", (req, res) => {
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("/*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+    });
+}
 
-    let path = req.params['0'].substring(1)
-
-    if (defended.includes(path)) {
-        // Return the actual file
-        res.sendFile(`${__dirname}../frontend/build/${path}`);
-    } else {
-        // Otherwise, redirect to /build/index.html
-        res.sendFile(`${__dirname}../frontend/build/index.html`);
-    }
-});
+else {
+    app.use(express.static(path.join(__dirname, '../frontend/public')));
+    app.get("/*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../frontend/public/index.html"));
+    });
+}
+// let defended = ['transformed.js', 'main.css', 'favicon.ico']
+//
+// app.get("*", (req, res) => {
+//
+//     let path = req.params['0'].substring(1)
+//
+//     if (defended.includes(path)) {
+//         // Return the actual file
+//         res.sendFile(`${__dirname}/build/${path}`);
+//     } else {
+//         // Otherwise, redirect to /build/index.html
+//         res.sendFile(`${__dirname}/build/index.html`);
+//     }
+// });
 app.listen(process.env.PORT || 5000, ()=> console.log(`Server running at port ${process.env.PORT}`));
