7629a0a326486c7e87531910ee0f6c044fe67b2e
humblezxc <you@example.com>
2022-10-28 19:58:59 +0200

    Changed Collection Show page styles

--- frontend/src/app/collections/CollectionShow.jsx
+++ frontend/src/app/collections/CollectionShow.jsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
 import {useEffect, useState} from "react";
 import axios from "axios";
-import { List, ListItem, ListItemText} from "@mui/material";
 import Button from "@mui/material/Button";
 import routes from "../../shared/constants/routes";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Typography } from "@mui/material";
+import Stack from '@mui/material/Stack';
+import Container from '@mui/material/Container';
+import Box from "@mui/material/Box";
 
 export default function CollectionShow() {
     const [collection, setCollection] = useState([]);
@@ -42,24 +52,83 @@ export default function CollectionShow() {
     };
 
     return (
-        <div>
-            <h1>Collection Show</h1>
-            <p>{collection.collectionName}</p>
-            <p>{collection.description}</p>
-            <Button key="edit" variant="contained" href={routes.COLLECTIONS + "/" + collection.id + "/edit"}>Edit</Button>
-            <Button key="delete" variant="contained" href={routes.COLLECTIONS + "/" + collection.id + "/delete"}>Delete</Button>
 
-            <Button key="create_item" variant="contained" href={routes.COLLECTIONS + "/" + collection.id + "/items/create"}>Add Item</Button>
-            <List>
-                {items.map((item) => (
-                    <ListItem key={item.id}>
-                        <ListItemText primary={item.itemName} secondary={item.description}/>
-                        <Button variant="contained" href={routes.COLLECTIONS + "/" + collection.id + "/items/" + item.id}>View</Button>
-                        <Button variant="contained" href={routes.COLLECTIONS + "/" + collection.id + "/" + item.id + "/edit"}>Edit</Button>
-                        <Button variant="contained" onClick={() => deleteItem(item.id)}>Delete</Button>
-                    </ListItem>
-                ))}
-            </List>
+        <div>
+            <Box
+                sx={{
+                    bgcolor: 'background.default',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Box
+                    sx={{
+                        pt:8,
+                        pb: 6,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography component="h1"
+                        variant="h2"
+                        align="center"
+                        color="text.primary"
+                        gutterBottom
+                    >
+                        Collection Show
+                    </Typography>
+                    <Container maxWidth="sm">
+                        <Typography variant="h4" align="center" color="text.secondary" paragraph>
+                            {collection.collectionName}
+                        </Typography>
+                        <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                            {collection.description}
+                        </Typography>
+                        <Stack
+                            sx={{ pt: 4 }}
+                            direction="row"
+                            spacing={2}
+                            justifyContent="center"
+
+                        >
+                            <Button  key="edit" variant="contained" href={routes.COLLECTIONS + "/" + collection.id + "/edit"}>Edit collection</Button>
+                            <Button key="delete" variant="outlined" href={routes.COLLECTIONS + "/" + collection.id + "/delete"}>Delete collection</Button>
+                            <Button key="create_item" variant="contained" href={routes.COLLECTIONS + "/" + collection.id + "/items/create"}>Add Item</Button>
+                        </Stack>
+                    </Container>
+                </Box>
+                <Container maxWidth="lg">
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 450 }} aria-label="simple table" align="center">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Items name</TableCell>
+                                    <TableCell></TableCell>
+                                    <TableCell align="right">View</TableCell>
+                                    <TableCell align="right">Edit</TableCell>
+                                    <TableCell align="right">Delete</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {items.map((item) => (
+                                    <TableRow
+                                        key={item.id}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                                <TableCell component="th" scope="row" >{item.itemName}</TableCell>
+                                                 <TableCell align="right"></TableCell>
+                                                <TableCell align="right"><Button variant="contained" href={routes.COLLECTIONS + "/" + collection.id + "/items/" + item.id}>View</Button></TableCell>
+                                                <TableCell align="right"><Button variant="outlined" href={routes.COLLECTIONS + "/" + collection.id + "/" + item.id + "/edit"}>Edit</Button></TableCell>
+                                                <TableCell align="right"><Button variant="contained" onClick={() => deleteItem(item.id)}>Delete</Button></TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Container>
+            </Box>
         </div>
     );
 }
